db1f5fd2cdcce9e63d60c999fe6fb6d959b68a7b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-04 20:14:55 +0700

    feat: add signalr script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(() => {
+    LoadData();
 
-// Write your JavaScript code.
+    const connection = new signalR.HubConnectionBuilder().withUrl("/signalrServer").build();
+    connection.start();
+
+    connection.on("LoadPosts", () => {
+        LoadData();
+    })
+
+    function LoadData() {
+        let tr = "";
+
+        $.ajax({
+            method: "GET",
+            url: "/Posts/GetPosts",
+            success: (result) => {
+                $.each(result, (k, v) => {
+                    tr += `<tr>
+                               <td>${v.Title}</td>
+                               <td>${v.Content}</td>
+                               <td>${v.PublishStatus}</td>
+                               <td>${v.PostCategory}</td>
+                               <td>${v.Author}</td>
+
+                               <td>
+                                   <a href='../Posts/Edit?id=${v.PostID}'>Edit</a> | 
+                                   <a href='../Posts/Details?id=${v.PostID}'>Details</a> | 
+                                   <a href='../Posts/Delete?id=${v.PostID}'>Delete</a>
+                               </td>
+                           </tr>`;
+                });
+                $("#tableBody").html(tr);
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
+    }
+})
