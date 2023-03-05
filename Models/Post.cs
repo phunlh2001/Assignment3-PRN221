2aeb0389b7f670126e1c364e7d7e1771163a8a05
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-05 14:43:31 +0700

    fix: update setter of create and update time

--- Models/Post.cs
+++ Models/Post.cs
@@ -12,12 +12,14 @@ namespace Assignment3.Models
         public int CategoryID { get; set; }
 
         [Column("created_date")]
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? CreatedDate { get; init; } = DateTime.Now;
         [Column("updated_date")]
-        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; private set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Title must be not empty!")]
         public string Title { get; set; }
         public string Content { get; set; }
-        public Status PublishStatus { get; set; } = Status.Published;
+        public Status? PublishStatus { get; set; } = Status.Published;
 
         public virtual PostCategory PostCategory { get; set; }
         public virtual AppUser Author { get; set; }
