4b70955f14dad98b52099b0dc5408eb1ed452804
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-06 05:09:19 +0700

    feat: add search

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using Assignment3.Extentions;
+using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Assignment3.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetData(string txt)
+        {
+            var post = _context.SearchByText(txt);
+
+            var category = _context.PostCategories
+                .FirstOrDefault(e => e.Description.Contains(txt));
+            var post2 = _context.Posts
+                .Include(p => p.Author)
+                .FirstOrDefault(p => p.PostCategory == category);
+
+            if (post != null)
+            {
+                return new JsonResult(post);
+            }
+            else if (category != null)
+            {
+                return new JsonResult(post2);
+            }
+            return NoContent();
+        }
+    }
+}
