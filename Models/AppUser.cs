8dc94424f868e9abb432d548a29a35f208d0882d
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-06 09:39:05 +0700

    fix: add validate

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -8,9 +8,13 @@ namespace Assignment3.Models
     {
         [Column("AuthorID")]
         [Key] public int ID { get; set; }
+        [Required(ErrorMessage = "Field name must be not empty")]
         public string FullName { get; set; }
+        [Required(ErrorMessage = "Address must be not empty")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Email must be not empty")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password must be not empty")]
         public string Password { get; set; }
 
         [ForeignKey("AuthorID")]
