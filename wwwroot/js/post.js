c899a8cdf5b9c40b4775d3d6d1fda52d9f0f4074
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-06 08:47:12 +0700

    feat: add post script for load post realtime

---
+++ wwwroot/js/post.js
@@ -0,0 +1,39 @@
+﻿$(() => {
+    LoadData();
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/signalrServer").build();
+    connection.start();
+
+    connection.on("LoadPosts", () => {
+        LoadData();
+    })
+
+    LoadData();
+
+    function LoadData() {
+        let tr = "";
+
+        $.ajax({
+            method: "GET",
+            url: "/Posts/GetPosts",
+            success: (result) => {
+                $.each(result, (k, v) => {
+                    tr += `<tr>
+                               <td>${v.title ?? ""}</td>
+                               <td>${v.createdDate ?? ""}</td>
+
+                               <td>
+                                   <a href='../Posts/Details?id=${v.id}' class="btn btn-secondary">
+                                        <i class="fa fa-eye"></i>
+                                   </a>
+                               </td>
+                           </tr>`;
+                });
+                $("#tableBody").html(tr);
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
+    }
+})
